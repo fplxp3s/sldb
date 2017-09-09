660dc1c25c6cd5b3b6a6b6358d4bd8514652a1fe
Vitor Marques <vitormarques.sa@gmail.com>
2017-09-08 23:35:23 -0300

    alteracao da classe de migration da tabela perfil

--- database/migrations/2017_08_29_183856_create_perfils_table.php
+++ database/migrations/2017_08_29_183856_create_perfils_table.php
@@ -15,7 +15,7 @@ class CreatePerfilsTable extends Migration
     {
         Schema::create('perfil', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('descricao', 255)->unique();
+            $table->string('descricao', 255);
         });
     }
 
